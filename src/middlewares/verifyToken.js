f63c06cf1900a0159414c1dd68cdd3487a1f59a9
Chirag <chiragmakwana1807@gmail.com>
2023-04-14 13:10:19 +0530

    token files added

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const secret_key_Access_Token = process.env.secret_key_Access_Token;
+
+module.exports = (req, res, next) => {
+    try{
+        console.log(req.headers['authorization']);
+        const accessToken = req.headers.authorization.split(" ")[1];
+        console.log(accessToken);
+        const id = jwt.verify(accessToken, secret_key_Access_Token);
+        if(id != null){
+            req.headers = id;
+            next();
+        }
+        else{
+            return res.status(401).json({
+                msg:"Token is invalid"
+            })
+        }
+    }
+    catch(error){
+        return res.status(401).json({
+            msg:"Invalid Token"
+        })
+    }
+}
