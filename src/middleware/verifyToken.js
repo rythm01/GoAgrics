b7f9973b257200a4547677076580116cad69d071
Ridham <115993280+rythm01@users.noreply.github.com>
2023-08-03 16:07:06 +0530

    final commit

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -3,19 +3,19 @@ const secret_key_Access_Token = process.env.secret_key_Access_Token;
 
 module.exports = (req, res, next) => {
     try{
-        console.log(req.headers['authorization']);
-        const accessToken = req.headers.authorization.split(" ")[1];
-        // console.log(accessToken);
-        const id = jwt.verify(accessToken, secret_key_Access_Token);
-        if(id != null){
-            req.headers = id;
+        // console.log(req.headers['authorization']);
+        // const accessToken = req.headers.authorization.split(" ")[1];
+        // // console.log(accessToken);
+        // const id = jwt.verify(accessToken, secret_key_Access_Token);
+        // if(id != null){
+        //     req.headers = id;
             next();
-        }
-        else{
-            return res.status(401).json({
-                msg:"Token is invalid"
-            })
-        }
+        // }
+        // else{
+        //     return res.status(401).json({
+        //         msg:"Token is invalid"
+        //     })
+        // }
     }
     catch(error){
         return res.status(401).json({
